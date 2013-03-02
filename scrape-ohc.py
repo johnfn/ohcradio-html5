47b61823ace791707aed6e5aef535d8848e03505
Grant Mathews <johnfn@gmail.com>
2013-03-02 14:24:36 -0800

    More or less done.
    The one thing I want to still do is to have a fallback flash mp3 player.

--- scrape-ohc.py
+++ scrape-ohc.py
@@ -8,21 +8,24 @@ import time
 # Given the HTML for a OHC page, attempts to grab the theme from that page.
 # Might fail on weird weeks.
 def find_theme(html):
-    theme = re.findall('(Tonights|Tonight\'s|Todays|Today\'s) theme is:? "(.*)"', html)
+    theme = re.findall('(Tonights|Tonight\'s|Todays|Today\'s|This weeks) theme (is|will be):? "(.*)"', html)
     if len(theme) > 0:
-        return theme[0][1]
+        return theme[0][2]
     return "???"
 
 # Given an OHC # and a ranking from the top of the page, returns the path to
 # the song at that rank.
-def get_ohc_url(which_ohc, which_rank):
+def get_ohc_url(which_ohc):
     idx = 0
+    result = ""
 
     # Download OHC page
     response = urllib2.urlopen('http://compo.thasauce.net/rounds/view/OHC%s' % which_ohc)
     html = response.read()
     
-    print "OHC%s, rank %d (Theme: %s)" % (which_ohc, which_rank + 1, find_theme(html))
+    result += "{"
+    result += "num: %s," % which_ohc
+    result += "theme: '%s'," % find_theme(html)
     
     # skip over "Materials" section, if there is one
     while True:
@@ -33,30 +36,33 @@ def get_ohc_url(which_ohc, which_rank):
             break
     
     url = ""
-    
+
     # Hack our way through the top 5
     for x in range(5):
         # hack the title
-        title = html.find('item_head">', idx) + len('item_head">\n')
-        endtitle = html.find('<span>', title)
-        print "Title: ", html[title:endtitle]
+        starttitle = html.find('item_head">', idx) + len('item_head">\n')
+        endtitle = html.find('<span>', starttitle)
+        title = html[starttitle:endtitle]
+        title = title.replace('"', '') 
+
+        result += 'title%d: "%s",' % (x, html[starttitle:endtitle])
 
         # hack the user
+        # beautifulsoup? what's that?
         user = html.find('">', endtitle) + len('">')
         enduser = html.find('</a>', user)
-        print "User: ", html[user:enduser]
+        result += "user%d: '%s'," % (x, html[user:enduser])
 
         idx = html.find('a href="/files/', idx + 1)
         endidx = html.find('">Download', idx)
         url = "http://compo.thasauce.net" + html[idx + len('a href="'):endidx ]
-        if x == which_rank:
-            return url
+        result += "url%d: '%s'" % (x, url)
+    result += "},"
 
-ohc_num = "050"
+    return result
 
-print get_ohc_url(ohc_num, 0)
-print get_ohc_url(ohc_num, 1)
-print get_ohc_url(ohc_num, 2)
-print get_ohc_url(ohc_num, 3)
-print get_ohc_url(ohc_num, 4)
-
+for ohc in range(229):
+  ohc_num = str(ohc).zfill(3)
+  json = get_ohc_url(ohc_num)
+  if len(json) > 10000: continue
+  print json
